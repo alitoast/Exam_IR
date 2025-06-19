7aa6316443399e484f6efa27b7c8ba8d99aeb2a5
alitoast <alice.macuz@gmail.com>
2025-06-19 21:08:14 +0200

    changed link

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def main():
     scheduler = Scheduler(max_concurrency, num_spiders, max_depth=2)
 
     logging.info("Running scheduler with seeds.")
-    await scheduler.run(seeds=["https://example.com/"])
+    await scheduler.run(seeds=["https://freakontrol.com/"])
     logging.info("Scheduler finished.")
 
 if __name__ == "__main__":
