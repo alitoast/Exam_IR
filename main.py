5d57c1d7581e7c0944b9acbbdb969cb08e782fa3
alitoast <alice.macuz@gmail.com>
2025-06-19 09:54:30 +0200

    main used do far for testing

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from storage import Storage
 from parser import Parser
 from fetcher import Fetcher
 from scheduler import Scheduler
+from config import setup_logging
+import utils_async
+import logging
+
+
+setup_logging()
+logger = logging.getLogger(__name__)
+
 
 async def main():
     # Download necessary NLTK data
@@ -12,8 +20,8 @@ async def main():
 
     # Initialize components
     storage = Storage()
-    #parser = Parser()
-    #fetcher = Fetcher()
+    parser = Parser()
+    fetcher = Fetcher()
     scheduler = Scheduler(max_concurrency=10, num_spiders=5)
 
     # Optionally: add initial seed URLs
