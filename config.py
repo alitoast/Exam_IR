781f155dae26b125c807185ec9160f5cdd8bc533
alitoast <alice.macuz@gmail.com>
2025-06-19 10:42:38 +0200

    broaded config logger for debug

--- config.py
+++ config.py
@@ -3,17 +3,17 @@ import logging
 def setup_logging():
     # create logger object
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)  # Set the base level for the logger
+    logger.setLevel(logging.DEBUG)  # Set the base level for the logger
 
     # create file handler
     file_handler = logging.FileHandler("crawler.log")
-    file_handler.setLevel(logging.INFO)
+    file_handler.setLevel(logging.DEBUG)
     file_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s", datefmt="%H:%M:%S")
     file_handler.setFormatter(file_formatter)
 
     # create console handler
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
+    console_handler.setLevel(logging.DEBUG)
     console_formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s", datefmt="%H:%M:%S")
     console_handler.setFormatter(console_formatter)
 
